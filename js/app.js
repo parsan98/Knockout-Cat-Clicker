5650341129265eb6e093bbeebdd2a448260b0e25
Parvathy Santhosh <parsan1998@gmail.com>
2018-09-15 16:09:43 +0100

    Separation of Model from ViewModel

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 var ViewModel = function () {
+  this.currentCat = ko.observable(new Cat());
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() +1);
+  };
+}
+
+var Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
   this.imgAttribution = ko.observable('SoonToBeDeleted');
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() +1);
-  };
   this.catLevel = ko.computed(function() {
     if (this.clickCount() < 10) {
       return "Neonate";
